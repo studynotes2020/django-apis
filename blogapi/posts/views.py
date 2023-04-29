15f5da3bfc58028e252ddd35a29125db0a84ab26
studynotes2020 <joonyi2006@hotmail.com>
2023-04-29 20:41:51 +0800

    Chapter 6 Permissions

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
@@ -10,5 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
